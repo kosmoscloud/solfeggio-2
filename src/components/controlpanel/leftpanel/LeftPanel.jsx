dd8e4f87fe68e96758b916f933f3014fd39422d4
kosmoscloud <maciej.komosa.14@gmail.com>
2024-12-15 14:45:22 +0100

    zmiana layoutu lewego panelu na flexbox

--- src/components/controlpanel/leftpanel/LeftPanel.jsx
+++ src/components/controlpanel/leftpanel/LeftPanel.jsx
@@ -9,13 +9,21 @@ function LeftPanel() {
 
     return <div className="left-panel">
             <PanelHeader text={"Ćwiczenie: "+exerciseName} number={0}/>
-            <PanelButton text="START / RESET" number={1} buttonid="startreset"/>
-            <PanelButton text="NASTĘPNY" number={2} buttonid="next"/>
-            <PanelButton text="POWTÓRZ" number={3} buttonid="repeat"/>
-            <PanelButton text="KONTYNUUJ" number={4} buttonid="continue"/>
-            <PanelButton text="COFNIJ NUTĘ" number={5} buttonid="undo"/>
-            <PanelButton text="WYJŚCIE" number={6} buttonid="exit"/>
-            <PanelButton text="PODPOWIEDZ" number={7} buttonid="hint"/>
+            <div className="buttons-panel">
+                <div className="buttons-column">
+                    <PanelButton text="START / RESET" number={1} buttonid="startreset"/>
+                    <PanelButton text="KONTYNUUJ" number={4} buttonid="continue"/>
+                    <PanelButton text="WYJŚCIE" number={6} buttonid="exit"/>
+                </div>
+                <div className="buttons-column">
+                    <div className="buttons-row">
+                        <PanelButton text="NASTĘPNY" number={2} buttonid="next"/>
+                        <PanelButton text="POWTÓRZ" number={3} buttonid="repeat"/>
+                    </div>
+                    <PanelButton text="COFNIJ NUTĘ" number={5} buttonid="undo"/>
+                    <PanelButton text="PODPOWIEDZ" number={7} buttonid="hint"/>
+                </div>
+            </div>
         </div>;
 }
 
